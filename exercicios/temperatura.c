9491b89f1606791ac8a57be93bf3439370dbfdee
Iohana Maria <122891414+IohanaViterbino@users.noreply.github.com>
2026-03-17 21:05:43 +0000

    refactor: escovando bit e melhorando o saída.

--- exercicios/temperatura.c
+++ exercicios/temperatura.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-float celsiusToFahrenheit(float celsius);
-float fahrenheitToCelsius(float fahrenheit);
-
 int main(int argc, char *argv[]) {
     if (argc < 3) {
-        fprintf(stderr, "Use: %s <C|F> <valor>\n", argv[0]);
+        fprintf(stderr, "Use: %s <C/F> <valor>\n", argv[0]);
         return 1;
     }
 
@@ -15,23 +12,14 @@ int main(int argc, char *argv[]) {
     float resultado;
 
     if (tipo == 'C' || tipo == 'c') {
-        resultado = celsiusToFahrenheit(valor);
-        printf("%.1f °C é igual a %.1f °F\n", valor, resultado);
+        resultado = (valor * 9.0 / 5.0) + 32.0;
     } else if (tipo == 'F' || tipo == 'f') {
-        resultado = fahrenheitToCelsius(valor);
-        printf("%.1f °F é igual a %.1f °C\n", valor, resultado);
+        resultado = (valor - 32.0) * 5.0 / 9.0;
     } else {
         fprintf(stderr, "Erro: Tipo inválido. Use 'C' para Celsius ou 'F' para Fahrenheit.\n");
         return 1;
     }
+    printf("%.1f °%c é igual a %.1f °C\n", valor, tipo, resultado);
 
     return 0;
-}
-
-float celsiusToFahrenheit(float celsius) {
-    return (celsius * 9.0 / 5.0) + 32.0;
-}
-
-float fahrenheitToCelsius(float fahrenheit) {
-    return (fahrenheit - 32.0) * 5.0 / 9.0;
 }
